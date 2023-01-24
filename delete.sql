023952329bc2a6759078298ff05fcd4b3f442c44
Isaac Vásquez Tucent <117482024+isaacvt01@users.noreply.github.com>
2023-01-24 10:58:21 +0100

    Add files via upload

---
+++ delete.sql
@@ -0,0 +1,11 @@
+-- Delete data from Shipments table
+DELETE FROM shipments;
+
+-- Delete Orders table data
+DELETE FROM orders;
+
+-- Delete Bikes table data
+DELETE FROM bikes;
+
+-- Delete data from Stores table
+DELETE FROM stores;
